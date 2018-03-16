0caa3d5f8c4749026f8b5bde840f8d2f3e4c0026
Alex Hart <alex@hartcode.com>
2018-03-15 21:04:34 -0500

    Add Quit option
    Reorganize UI elements

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -28,7 +28,7 @@ public class MenuHandler : MonoBehaviour {
 		// Update is called once per frame
 	public void Show () {
 		hidden = false;
-			Menu.SetActive (true);
+			Menu.SetActive (true); 
 		Gamestate.ShowEverything (false);
 	}
 
@@ -55,6 +55,18 @@ public class MenuHandler : MonoBehaviour {
 		public void AchievementsButtonClick() {
 			Debug.Log ("AchievementsButtonClick");
 		}
+
+		public void QuitButtonClick() {
+			Debug.Log ("QuitButtonClick");
+			#if UNITY_EDITOR
+			// Application.Quit() does not work in the editor so
+			// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+			Application.Quit();
+			#endif
+		}
+
 
 }
 }
