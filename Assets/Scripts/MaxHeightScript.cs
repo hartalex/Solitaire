6cf4417bb6d87061d6be0ad1696fa580824804e9
Alex Hart <alex@hartcode.com>
2018-12-29 14:34:57 -0600

    Unity Version Bump
    Added MaxHeight Script to measure the height of the tables and cards

---
+++ Assets/Scripts/MaxHeightScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Direction {
+	x,y,z}
+
+public class MaxHeightScript : MonoBehaviour {
+
+	public double maximumHeight = double.MinValue;
+	public Text uiText;
+	public Direction direction = Direction.z;
+	// Use this for initialization
+	void Start () {
+		maximumHeight = double.MinValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		double height;
+		switch(direction) {
+			case Direction.x: 
+				height = transform.position.x;
+				break;
+			case Direction.y: 
+				height = transform.position.y;
+				break;
+		default:
+			case Direction.z: 
+				height = transform.position.z;
+				break;
+		}
+		if (height > maximumHeight) {
+			maximumHeight = height;
+			uiText.text = maximumHeight.ToString("F");
+		}
+	}
+}
